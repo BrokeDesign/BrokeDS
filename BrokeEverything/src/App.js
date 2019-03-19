bc6b32cea581f634623c7ab5abfe5091c2913efe
Christian Gorke <cgorke@aplura.com>
2019-03-18 18:23:42 -0600

    Built in grid, started setting up some components for demoing, having issues with card components styles from scss lib.

--- BrokeEverything/src/App.js
+++ BrokeEverything/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Footer, Header} from './components/layout';
+import {Col, Grid, Row} from './components/grid';
+import { Card, CardContent } from './components/card';
 
 const App = () => {
     return (
         <div>
         <Header>
-            <h1>Grid</h1>
+            <h1>BrokeDS - Broke Everything</h1>
         </Header>
-        <section>
-            <h2>Example with even row & column classes</h2>
-        </section>
+        <Grid>
+            <Row border="outline">
+                <Col colSize={12}>
+                    <Card container="card-container">
+                        col-12
+                    </Card>
+                </Col>
+            </Row>
+        </Grid>
+        
         <Footer><h1>Footer</h1></Footer>
         </div>
     );
--- BrokeEverything/src/components/grid/Grid.js
+++ BrokeEverything/src/components/grid/Grid.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Grid = (props) => {
     return(
-        <div>{props.children}</div>
+        <div className="container">{props.children}</div>
     );
 }
 
