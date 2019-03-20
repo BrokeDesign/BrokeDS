1fca7c2ea8e72b1a2354377dba724416b56182e5
Christian Gorke <cgorke@aplura.com>
2019-03-20 13:08:21 -0600

    Removed unneeded dependencies and started work on adding components and theme-ing

--- BrokeEverything/src/App.js
+++ BrokeEverything/src/App.js
@@ -2,40 +2,77 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Footer, Header } from "./components/layout";
 import { Col, Grid, Row } from "./components/grid";
-import { Card, CardContent } from "./components/card";
+import { Card, CardContent, CardFoot, CardHead } from "./components/card";
 
 const App = () => {
     return (
-        <div className="container">
-            <div className="container-head">
-                <div className="title">Contiuous Grid (One row)</div>
-                <hr />
-            </div>
+        <div>
+        <Header>
+            Header
+        </Header>
+        <article className="container">
+            <h2>Grid</h2>
+            <hr/>
+            <Grid>
+                <Row>
+                    <Col>
 
-            <div className="row">
-                <div className="col-12">Col-12</div>
-                <div className="col-11">Col-11</div>
-                <div className="col-1">Col-1</div>
-                <div className="col-10">Col-10</div>
-                <div className="col-2">Col-2</div>
-                <div className="col-9">Col-9</div>
-                <div className="col-3">Col-3</div>
-                <div className="col-8">Col-8</div>
-                <div className="col-4">Col-4</div>
-                <div className="col-7">Col-7</div>
-                <div className="col-5">Col-5</div>
-                <div className="col-6">Col-4</div>
-                <div className="col-6">Col-4</div>
-                <div className="col-5">Col-5</div>
-                <div className="col-5">Col-5</div>
-                <div className="col-2">Col-2</div>
-                <div className="col-3">Col-3</div>
-                <div className="col-3">Col-3</div>
-                <div className="col-3">Col-3</div>
-                <div className="col-3">Col-3</div>
-                <div className="col-4">Col-4</div>
-                <div className="col-4 col-offset-4">Col-4 offset 4</div>
+                    </Col>
+                    <Col colSize="6">
+                        colSize="6"
+                    </Col>
+                    <Col colSize="6">
+                        colSize="6"
+                    </Col>
+                    <Col colSize="3">
+                        colSize="3"
+                    </Col>
+                    <Col colSize="9">
+                        colSize="9"
+                    </Col>
+                    <Col colSize="6" offset="col-offset-6">
+                        colSize="6" offset="col-offset-6"
+                    </Col>
+                </Row>
+            </Grid>
+            <br/>
+            <br/>
+            <h2>Cards</h2>
+            <Card>
+                <CardHead>
+                    Card Head
+                </CardHead>
+                <CardContent>
+                Card Content
+                </CardContent>
+                <CardFoot>
+                    Card Foot
+                </CardFoot>
+            </Card>
+            <br/><br/>
+            <h2>Buttons</h2>
+            <div className="wrapper">
+            <button type="button" class="button-alpha">
+                button-alpha
+            </button>
+            <button type="button" class="button-beta">
+                button-beta
+            </button>
+            <button type="button" class="button-gamma">
+                button-gamma
+            </button>
+            <button type="button" class="button-psi">
+                button-psi
+            </button>
+            <button type="button" class="button-omega">
+                button-omega
+            </button>
+
             </div>
+        </article>
+        <Footer>
+            Footer
+        </Footer>
         </div>
     );
 };
